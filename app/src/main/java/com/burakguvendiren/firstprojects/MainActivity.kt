6cc997ae064e783908c363cd5246d383db57b2d1
Burak Güvendiren <guvendirenn@gmail.com>
2023-01-01 21:02:16 +0300

    Learned Constants

--- app/src/main/java/com/burakguvendiren/firstprojects/MainActivity.kt
+++ app/src/main/java/com/burakguvendiren/firstprojects/MainActivity.kt
@@ -9,14 +9,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println ("Hello  World")
+
+        //değişkenler ve sabitler
         println (5 * 10)
         var a = 5
         var b = 10
         println (a * b)
-        var old = 50
+        var old = 60
         println (old * 10 / 7)
-        old = 15
-        println (old * 10 / 7)
+        old = 60
+        println (old * 5 / 8)
+
+        val x = 5
+        val y = 20
+
+        println(x+y)
+
+
+        // camelCase
+        // snake_case
+
+        val yasSonucu = old * x
+        println(yasSonucu)
+
+
 
 
     }
