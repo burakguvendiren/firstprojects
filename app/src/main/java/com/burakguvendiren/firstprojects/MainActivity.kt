1077fc08250b9a92990bc11330ddc16bc02a682b
Burak Güvendiren <guvendirenn@gmail.com>
2023-01-01 20:11:34 +0300

    Learned Variables

--- app/src/main/java/com/burakguvendiren/firstprojects/MainActivity.kt
+++ app/src/main/java/com/burakguvendiren/firstprojects/MainActivity.kt
@@ -7,5 +7,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println ("Hello  World")
+        println (5 * 10)
+        var a = 5
+        var b = 10
+        println (a * b)
+        var old = 50
+        println (old * 10 / 7)
+        old = 15
+        println (old * 10 / 7)
+
+
     }
 }
