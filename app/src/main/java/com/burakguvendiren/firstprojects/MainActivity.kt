36af244635e3939d34f3f8ce22132ddd84a2f262
Burak Güvendiren <guvendirenn@gmail.com>
2023-01-03 17:52:03 +0300

    Learned Set

--- app/src/main/java/com/burakguvendiren/firstprojects/MainActivity.kt
+++ app/src/main/java/com/burakguvendiren/firstprojects/MainActivity.kt
@@ -2,33 +2,34 @@ package com.burakguvendiren.firstprojects
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println ("Hello  World")
+        println("Hello  World")
 
         //değişkenler ve sabitler
         //Integer
 
-        println ("-----Int-----")
+        println("-----Int-----")
 
 
-        println (5 * 10)
-        var a = 5
-        var b = 10
-        println (a * b)
+        println(5 * 10)
+        val a = 5
+        val b = 10
+        println(a * b)
         var old = 60
-        println (old * 10 / 7)
+        println(old * 10 / 7)
         old = 60
-        println (old * 5 / 8)
+        println(old * 5 / 8)
 
         val x = 5
         val y = 20
 
-        println(x+y)
+        println(x + y)
 
 
         // camelCase
@@ -38,33 +39,33 @@ class MainActivity : AppCompatActivity() {
         println(yasSonucu)
 
         //Tanımlama (Defining)
-        val benimIntegerim : Int
+        val benimIntegerim: Int
 
         //Başlatma, Değerini Atama (Initializing, Inilitiazation)
         benimIntegerim = 5
-        var yeniInteger : Int = 20
+        var yeniInteger: Int = 20
         println(benimIntegerim / 2)
 
         //Long
-        println ("-----Int-----")
+        println("-----Int-----")
 
-        var myLong : Long = 100
+        var myLong: Long = 100
         myLong = 30000000
         println(myLong)
 
         //Double & Float
-        println ("-----Double & Float-----")
+        println("-----Double & Float-----")
         val pi = 3.14
-        println(pi*2)
+        println(pi * 2)
 
-        val floatPi : Float = 3.14f
+        val floatPi: Float = 3.14f
         println(floatPi * 2)
         val yeniDouble = 5.0
         println(yeniDouble / 2)
 
 
         //String - Metin
-        println ("-----String - Metin-----")
+        println("-----String - Metin-----")
 
         val benimString = "Benim Yeni Metnim"
         println(benimString.length)
@@ -74,14 +75,14 @@ class MainActivity : AppCompatActivity() {
         val tamisim = isim + " " + soyisim
         println(tamisim)
 
-        val yeniBirString : String
+        val yeniBirString: String
         yeniBirString = "10"
 
         val baskaBirString = "20"
-        println(yeniBirString+baskaBirString)
+        println(yeniBirString + baskaBirString)
 
         //boolean
-        println ("-----Boolean-----")
+        println("-----Boolean-----")
 
         var benimBoolean = true
         benimBoolean = false
@@ -94,11 +95,11 @@ class MainActivity : AppCompatActivity() {
         // && -> ve
         // || -> veya
 
-        println(3<5)
-        println(4!=4)
+        println(3 == 5)
+        println(4 != 4)
 
         //Veri Tipi Dönüştürme
-        println ("-----Dönüştürme-----")
+        println("-----Donusturme-----")
 
         val benimInt = 10
         val benimLongaCevrilenInt = benimInt.toLong()
@@ -111,47 +112,80 @@ class MainActivity : AppCompatActivity() {
         //Koleksiyonlar
 
         //Array - Dizi
-        println ("-----Dizi-----")
-
+        println("-----Dizi-----")
         val stringOrnegi = "Burak"
-
-        val benimDizim = arrayOf(stringOrnegi,"Burak","Güvendiren","Marcus","Aurelius")
+        val benimDizim = arrayOf(stringOrnegi,"Güvendiren","Marcus","Aurelius","Stoa")
 
         //index
         println(benimDizim[0])
         println(benimDizim.get(1))
         println(benimDizim.get(2))
-        benimDizim[2] = "Mahmut"
+        benimDizim[2] = "Stoic"
         println(benimDizim.get(2))
-        benimDizim.set(3,"Mehmet")
+        benimDizim.set(3, "Seneca")
         println(benimDizim[3])
 
-        val numaraDizisi = doubleArrayOf(1.0,2.0,3.0)
+        val numaraDizisi = doubleArrayOf(1.0,2.0,3.5)
         println(numaraDizisi.get(2))
 
-        val karisikDizi = arrayOf("Burak,24,16.5,true,false")
+        val karisikDizi = arrayOf("Burak",24,16.5,true,false)
         println(karisikDizi.get(3))
 
         //ArrayList - Listeler
-        println ("-----ArrayList-----")
 
-        val isimListesi = arrayListOf<String>("Burak","Güvendiren","Stoa")
+        println("-----ArrayList-----")
+
+        val isimListesi = arrayListOf<String>("Burak","Güvendiren","Marcus")
+
         println(isimListesi[0])
-        println(isimListesi.get(1))
+        println(isimListesi[1])
 
-        isimListesi.add("Mehmet")
-        isimListesi.add("Atlas")
+        isimListesi.add("Seneca")
+        isimListesi.add("Aurelius")
         println(isimListesi.get(4))
 
         val karisikArrayList = arrayListOf<Any>()
         karisikArrayList.add("Burak")
-        karisikArrayList.add(5)
-        karisikArrayList.add(true)
+        karisikArrayList.add("5")
+        karisikArrayList.add("true")
+        println(karisikArrayList)
 
         val intArrayList = ArrayList<Int>()
         intArrayList.add(5)
         intArrayList.add(20)
         println(intArrayList.get(1))
 
+        //Set
+
+        println("-----Set-----")
+
+        val ornekDizi = arrayOf(7,8,9,9,9,8,10)
+        println("index 2: $(ornekDizi[2]")
+        println("index 3: $(ornekDizi[3]")
+
+        println(ornekDizi.size)
+
+        val benimSet = setOf<Int>(7,8,9,9,9,8,10)
+        println(benimSet.size)
+
+        //For Each
+        benimSet.forEach {
+            println(it)
+        }
+
+        val digerSet = HashSet<String>()
+        digerSet.add("Burak")
+        digerSet.add("Burak")
+        digerSet.add("Burak")
+        digerSet.add("Güvendiren")
+
+        digerSet.forEach {
+            println(it)
+        }
+
+
+
     }
-}
+
+
+    }
